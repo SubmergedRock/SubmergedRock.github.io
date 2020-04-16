ba042b7c46781558da1f24c4c50ae93a9ceb9614
Evan Michael Bell <evanbell777@yahoo.com>
2020-04-16 17:11:05 -0500

    Update myscripts.js

--- css/myscripts.js
+++ css/myscripts.js
@@ -3,8 +3,8 @@ function Toggleside() {
   var menu = document.getElementById("side-menu")
   var links = document.getElementsByTagName("li")
   if (checkbox.checked == true){
-      menu.style.width = "10vw"
-      menu.style.height = "25vh"
+      menu.style.width = "40px"
+      menu.style.height = "170px"
       for (var g = 0; g<links.length; g++){
           links[g].style.opacity = "100"
       }
