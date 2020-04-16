b171413ed083ca14bf1165815404dd88e8a96c9f
Evan Michael Bell <evanbell777@yahoo.com>
2020-04-16 16:25:00 -0500

    Create myscripts.js

---
+++ css/myscripts.js
@@ -0,0 +1,9 @@
+function Toggleside() {
+  var menu = document.getElementById("side-menu")
+  if (menu.style.display = "block"){
+      menu.style.display = "none";
+  } else {
+     menu.style.display = "block"
+      
+  }
+}
